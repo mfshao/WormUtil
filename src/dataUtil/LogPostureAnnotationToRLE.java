9b72c5abbb46ac0df996deeeb8cec92a3c89e0c9
MSHAO1 <MSHAO1@MEDIXWS3.cstcis.cti.depaul.edu>
2017-06-29 16:14:42 -0500

    new

--- src/dataUtil/LogPostureAnnotationToRLE.java
+++ src/dataUtil/LogPostureAnnotationToRLE.java
@@ -30,8 +30,8 @@ import java.util.logging.Logger;
  */
 public class LogPostureAnnotationToRLE {
 
-    private static final String IN_FILE_NAME = "D:\\ContourAndSkel.csv";
-    private static final String OUT_FILE_NAME = "D:\\ContourAndSkel_output_RLEFeatures.csv";
+    private static final String IN_FILE_NAME = "D:\\Smooth_N2_nf4.csv";
+    private static final String OUT_FILE_NAME = "D:\\Smooth_N2_nf4_RLEFeatures.csv";
     private static final String[] STATES = {"Forward-NTD", "Forward-Sharp", "Forward-Shallow", "Backward-ReverseShort", "Backward-ReverseLong", "Stopped-ReverseShort", "Stopped-ReverseLong", "Stopped-Stop"};
 
     private static ArrayList<PostureAnnotation> loadData() {
@@ -44,12 +44,45 @@ public class LogPostureAnnotationToRLE {
         }
         return null;
     }
+    
+    private static ArrayList<RLEFeature> normalizeRLEFeatures(ArrayList<RLEFeature> rleFeatureList) {
+        float[] maxs = new float[11];
+        float[] mins = new float[11];
+        
+        for (int i = 0 ; i < 11; i++) {
+            maxs[i] = Float.MIN_VALUE;
+            mins[i] = Float.MAX_VALUE;
+        }
+        
+        rleFeatureList.stream().map((rf) -> rf.getRawValues()).forEach((rawValueList) -> {
+            for (int i = 0; i < 11; i++) {
+                if (maxs[i] < rawValueList.get(i)) {
+                    maxs[i] = rawValueList.get(i);
+                }
+                if (mins[i] > rawValueList.get(i)) {
+                    mins[i] = rawValueList.get(i);
+                }
+            }
+        });
+        
+        rleFeatureList.stream().forEach((rf) -> {
+            rf.setSre((rf.getSre() - mins[0]) / (maxs[0] - mins[0]));
+            rf.setLre((rf.getLre()- mins[1]) / (maxs[1] - mins[1]));
+            rf.setLare((rf.getLare() - mins[2]) / (maxs[2] - mins[2]));
+            rf.setHare((rf.getHare() - mins[3]) / (maxs[3] - mins[3]));
+            rf.setSrlae((rf.getSrlae() - mins[4]) / (maxs[4] - mins[4]));
+            rf.setSrhae((rf.getSrhae() - mins[5]) / (maxs[5] - mins[5]));
+            rf.setLrlae((rf.getLrlae() - mins[6]) / (maxs[6] - mins[6]));
+            rf.setLrhae((rf.getLrhae() - mins[7]) / (maxs[7] - mins[7]));
+            rf.setAln((rf.getAln() - mins[8]) / (maxs[8] - mins[8]));
+            rf.setRln((rf.getRln() - mins[9]) / (maxs[9] - mins[9]));
+            rf.setRp((rf.getRp() - mins[10]) / (maxs[10] - mins[10]));
+        });
+        
+        return rleFeatureList;
+    }
 
     public static void main(String[] args) {
-//        HashMap<String, ArrayList<RLEData>> allRLEList = new HashMap();
-//        HashMap<String, Integer> maxCount = new HashMap();
-//        int nr = 0;
-//        int np = 0;
 
         try {
             CSVWriter csv = new CSVWriter(new FileWriter(OUT_FILE_NAME), CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
@@ -57,6 +90,10 @@ public class LogPostureAnnotationToRLE {
 
         ArrayList<RLEFeature> rleFeatureList = new ArrayList();
         ArrayList<PostureAnnotation> csvData = loadData();
+        
+        for (PostureAnnotation pa : csvData) {
+            System.out.println(pa.getTitle()+"  "+pa.getValue().size());
+        }
 
         for (PostureAnnotation pa : csvData) {
             String title = pa.getTitle();
@@ -86,15 +123,10 @@ public class LogPostureAnnotationToRLE {
             }
 
             row = result.toArray(new String[0]);
-            
-            
-//                csv.writeNext(row);
         }
-//            csv.close();
-
-//        } catch (IOException ex) {
-//            Logger.getLogger(LogPostureAnnotation.class.getName()).log(Level.SEVERE, null, ex);
-//        }
+        
+//        rleFeatureList = normalizeRLEFeatures(rleFeatureList);
+        
         for (RLEFeature f : rleFeatureList) {
             String[] output = f.toOutput();
             csv.writeNext(output);
