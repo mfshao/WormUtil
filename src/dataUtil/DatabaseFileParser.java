2a615582ef275c0a133687494938c2c3cc0aded3
MSHAO1 <MSHAO1@MEDIXWS3.cstcis.cti.depaul.edu>
2017-11-09 23:08:44 -0600

    straintypeid table finished

--- src/dataUtil/DatabaseFileParser.java
+++ src/dataUtil/DatabaseFileParser.java
@@ -28,11 +28,12 @@ public class DatabaseFileParser {
     static final String[] TABLE_NAMES = new String[] {"Straintype", "ImageInfo", "ExperimentalFeatures", "FeatureLog", "HeadTailInfo", "LogDat", "RawMovementFeatures", "MovementFeaturesBinned", "Occupancy", "PostureFeatures", "ReferenceFeatures", "SegmentCentroid", "SizeandShapeFeatures", "Tracker", "TrajectoryFeatures", "VideoInfo"};
     
     public DatabaseFileParser() {
-        new File("\\\\CDM-MEDIXSRV\\Nematodes\\data\\*****\\dbtables").mkdir();
+        new File(OUTPUT_PATH.replace("*****", CATAGORY_NAME)).mkdir();
     }
     
     private void createAllDBTables() {
         createStraintype();
+        createImageInfo();
     }
     
     private void createStraintype() {
@@ -40,26 +41,59 @@ public class DatabaseFileParser {
             final String[] outputHeaders = new String[] {"StrainTypeid", "WormType", "ResolutionType", "FoodCondition", "SIndex"};
             final String outputFileName = "Straintype.csv";
             final String outputFilePath = OUTPUT_PATH.replace("*****", CATAGORY_NAME) + "\\" + outputFileName;
+            File outputFile = new File(outputFilePath);
+            outputFile.createNewFile();
 
             final Appendable outputFileWriter = new FileWriter(outputFilePath);
-            final CSVPrinter printer = CSVFormat.DEFAULT.withHeader(MasterFileCreater.MasterFileHeaders.class).print(outputFileWriter);
+            final CSVPrinter printer = CSVFormat.DEFAULT.withHeader(outputHeaders).print(outputFileWriter);
             
             String[] decompositedCName = CATAGORY_NAME.split("_");
-            String StrainTypeid = "";
-            String WormType = "";
-            String ResolutionType = "";
-            String FoodCondition = "";
-            String SIndex = "";
+            String strainTypeid = "";
+            String wormType = "";
+            String resolutionType = "";
+            String foodCondition = "";
+            String sIndex = "";
+            
+            if (decompositedCName.length > 0) {
+                wormType = decompositedCName[0];
+                
+                String fCAndSI = "";
+                if (decompositedCName.length == 2) {
+                    resolutionType = "LR";
+                    fCAndSI = decompositedCName[1];
+                } else if (decompositedCName.length == 3) {
+                    resolutionType = "HR";
+                    fCAndSI = decompositedCName[2];
+                }
+                
+                if (!fCAndSI.isEmpty()) {
+                    foodCondition = fCAndSI.replaceAll("[^A-Za-z]", "");
+                    sIndex = fCAndSI.replaceAll("[^0-9]", "");
+                }
+                
+                strainTypeid = wormType + "_" + resolutionType + "_" + foodCondition + "_" + sIndex;
+            } else {
+                System.out.println("CATAGORY_NAME parse fails");
+                return;
+            }
             
             List<String> outputFileData = new ArrayList<>();
-            outputFileData = Arrays.asList(outputHeaders);
+            outputFileData.add(strainTypeid);
+            outputFileData.add(wormType);
+            outputFileData.add(resolutionType);
+            outputFileData.add(foodCondition);
+            outputFileData.add(sIndex);
             printer.printRecord(outputFileData);
             printer.flush();
-            outputFileData.clear();
             
+            System.out.println("========= StrainTypeId creat completed ==========");
         } catch (IOException ex) {
             Logger.getLogger(DatabaseFileParser.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private void createImageInfo() {
+        
     }
     
     public static void main(String[] args) {
